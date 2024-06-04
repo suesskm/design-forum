f87c5b54c07c0c7aaf17dce9b4b0d522ae66b383
suesskm <157542248+suesskm@users.noreply.github.com>
2024-06-04 10:02:46 -0500

    blog

---
+++ js/blog.js
@@ -0,0 +1,11 @@
+// // Function to handle icon click event
+// function openPopup(imageUrl) {
+//     // Open a new window with the specified image URL
+//     window.open(imageUrl, '_blank', 'width=600,height=400');
+// }
+
+// // Add click event listener to icon
+// document.getElementById("icon").addEventListener('click', function() {
+//     // Call the openPopup function with the URL of the image you want to display
+//     openPopup('media/images/popup1.png');
+// });
